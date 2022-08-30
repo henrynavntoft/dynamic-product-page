ab54f4bcebc6953f404bd58788b67c1d71cb2997
henrynavntoft <henrylnavntoft@gmail.com>
2022-08-30 13:17:53 +0200

    save

--- product-list.js
+++ product-list.js
@@ -10,4 +10,20 @@ function handleProductList(data) {
   data.forEach(showProduct);
 }
 
-function showProduct(product) {}
+function showProduct(product) {
+  //Selecting template and cloning
+  const temp = document.querySelector("template").content;
+  const clone = temp.cloneNode(true);
+  // Change stuff
+  clone.querySelector("h2.productName").textContent =
+    product.productdisplayname;
+  clone.querySelector("p.brand").textContent = product.brandname;
+  clone.querySelector("p.price").textContent = product.price + ",-";
+  clone.querySelector(
+    "img.listImg"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  //Selection where i want the clone
+  const parent = document.querySelector("main");
+  //Appending it
+  parent.appendChild(clone);
+}
