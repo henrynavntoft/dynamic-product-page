c09f53a09a7d8653552596dab630dab5fd3ebbac
henrynavntoft <henrylnavntoft@gmail.com>
2022-08-30 11:26:30 +0200

    starting the js

--- product-list.js
+++ product-list.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+// Just checking
+function handleProductList(data) {
+  console.log(data);
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {}
